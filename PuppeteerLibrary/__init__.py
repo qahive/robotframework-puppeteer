de2bc60362bc9a57f186252bf45c584c569d80ad
atthaboons <atthaboon.s@gmail.com>
2020-06-18 22:24:16 +0700

    Move broswer to browser management function

--- PuppeteerLibrary/__init__.py
+++ PuppeteerLibrary/__init__.py
@@ -1,8 +1,7 @@
 import asyncio
-from pyppeteer.browser import Browser
-from robot.api.deco import not_keyword
-from PuppeteerLibrary.base.robotlibcore import keyword, DynamicCore
 from PuppeteerLibrary.custom_elements.SPage import SPage
+from robot.api.deco import not_keyword
+from PuppeteerLibrary.base.robotlibcore import DynamicCore
 from PuppeteerLibrary.keywords import (
     BrowserManagementKeywords,
     ElementKeywords,
@@ -48,33 +47,7 @@ class PuppeteerLibrary(DynamicCore):
         DynamicCore.__init__(self, libraries)
 
     @not_keyword
-    def get_browser(self) -> Browser:
-        return self.browser
-
-    @not_keyword
     def get_current_page(self) -> SPage:
         page = self.current_page
         page.__class__ = SPage
         return page
-
-    @keyword
-    def close_browser(self):
-        self.loop.run_until_complete(self.close_browser_async())
-        print('close')
-
-    @not_keyword
-    async def close_browser_async(self):
-        await self.browser.close()
-
-    @keyword
-    def maximize_browser_window(self):
-        """
-        Maximize view port not actual browser and set default size to 1366 x 768
-        """
-        self.loop.run_until_complete(self.maximize_browser_window_async())
-
-    async def maximize_browser_window_async(self):
-        await self.get_current_page().setViewport({
-            'width': 1366,
-            'height': 768
-        })
--- PuppeteerLibrary/keywords/browsermanagement.py
+++ PuppeteerLibrary/keywords/browsermanagement.py
@@ -1,4 +1,6 @@
 from pyppeteer import launch
+from pyppeteer.browser import Browser
+from robot.api.deco import not_keyword
 from PuppeteerLibrary.base.librarycomponent import LibraryComponent
 from PuppeteerLibrary.base.robotlibcore import keyword
 
@@ -12,19 +14,37 @@ class BrowserManagementKeywords(LibraryComponent):
                 'width': 1366,
                 'height': 768
             })
-            self.current_page = await self.browser.newPage()
-            await self.current_page.goto('https://www.w3schools.com/html/html_forms.asp')
-            await self.current_page.screenshot({'path': 'example.png'})
+            self.ctx.current_page = await self.browser.newPage()
+            await self.ctx.current_page.goto('https://www.w3schools.com/html/html_forms.asp')
+            await self.ctx.current_page.screenshot({'path': 'example.png'})
         self.loop.run_until_complete(open_browser_async())
 
-    '''
+    @not_keyword
+    def get_browser(self) -> Browser:
+        return self.browser
+
+    @keyword
+    def close_browser(self):
+        async def close_browser_async():
+            await self.browser.close()
+        self.loop.run_until_complete(close_browser_async())
+
+    @keyword
+    def maximize_browser_window(self):
+        """
+        Maximize view port not actual browser and set default size to 1366 x 768
+        """
+        async def maximize_browser_window_async():
+            await self.ctx.get_current_page().setViewport({
+                'width': 1366,
+                'height': 768
+            })
+        self.loop.run_until_complete(maximize_browser_window_async())
+
     @keyword
     def close_all_browsers(self):
         print('')
 
-    @keyword
-    def close_browser(self):
-        print('')
 
     @keyword
     def switch_browser(self, index_or_alias):
@@ -53,4 +73,3 @@ class BrowserManagementKeywords(LibraryComponent):
     @keyword
     def reload_page(self):
         print('')
-    '''
