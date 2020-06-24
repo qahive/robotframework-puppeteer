4c04417c00fcb5af6777ba79fbc4079ba2b28528
atthaboons <atthaboon.s@gmail.com>
2020-06-24 07:14:24 +0700

    Add wait for response url

--- Examples/wait-for-demo.robot
+++ Examples/wait-for-demo.robot
@@ -8,3 +8,9 @@ Demo wait for element
     Open browser    https://www.w3schools.com/html/html_forms.asp   options=${options}
     Wait Until Page Contains Element    css:h1
     Run Keyword And Expect Error    STARTS: TimeoutError:    Wait Until Page Contains Element    css:no_element    timeout=5s
+
+Demo wait for request and response
+    &{options} =    create dictionary   headless=${False}
+    Open browser    https://www.w3schools.com/js/js_ajax_intro.asp   options=${options}
+    Click Element    css:#demo button
+    Wait for response url    https://www.w3schools.com/js/ajax_info.txt
--- PuppeteerLibrary/keywords/waiting.py
+++ PuppeteerLibrary/keywords/waiting.py
@@ -5,8 +5,23 @@ from PuppeteerLibrary.base.robotlibcore import keyword
 class WaitingKeywords(LibraryComponent):
 
     @keyword
+    def wait_for_request_url(self, url, method, timeout=None):
+        """Wait for request url"""
+        async def wait_for_request_async():
+            await self.ctx.get_current_page().waitForRequest(lambda req: req.url == url and req.method == method, timeout)
+        self.loop.run_until_complete(wait_for_request_async())
+
+    @keyword
+    def wait_for_response_url(self, url, status=200, timeout=None):
+        """Wait for response url"""
+        async def wait_for_response_async():
+            await self.ctx.get_current_page().waitForResponse(lambda res: res.url == url and res.status == status, timeout)
+        self.loop.run_until_complete(wait_for_response_async())
+
+    @keyword
     def wait_until_page_contains_element(self, locator, timeout=None):
         """Wait until page contains element within specific timeout"""
         async def wait_until_page_contains_element_async():
             await self.ctx.get_current_page().waitForSelector_with_selenium_locator(locator, timeout)
         self.loop.run_until_complete(wait_until_page_contains_element_async())
+
