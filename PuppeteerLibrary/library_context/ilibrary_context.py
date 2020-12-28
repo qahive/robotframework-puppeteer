04186005961d8c162c8c2de140154de6b26b1b88
atthaboons <atthaboon.s@gmail.com>
2020-12-28 16:07:16 +0700

    Fix screenshot path issue when set screenshot directory

--- PuppeteerLibrary/library_context/ilibrary_context.py
+++ PuppeteerLibrary/library_context/ilibrary_context.py
@@ -1,14 +1,17 @@
-from PuppeteerLibrary.custom_elements.base_page import BasePage
+import os
 from abc import ABC, abstractmethod
+from PuppeteerLibrary.custom_elements.base_page import BasePage
 
 
 class iLibraryContext(ABC):
 
     browser_type: str = None
+    screenshot_path: str = None
     timeout: int = 30
 
     def __init__(self, browser_type: str):
         self.browser_type = browser_type
+        self.screenshot_path = os.curdir
     
     @abstractmethod
     async def start_server(self, options: dict=None):
@@ -53,3 +56,11 @@ class iLibraryContext(ABC):
     @abstractmethod
     def get_async_keyword_group(self, keyword_group_name: str):
         pass
+
+    def set_screenshot_path(self, path: str):
+        self.screenshot_path = path
+        if not os.path.exists(self.screenshot_path):
+            os.makedirs(self.screenshot_path)
+
+    def get_screenshot_path(self):
+        return self.screenshot_path
